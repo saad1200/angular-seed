1f3acd8444d7e3cfc76c8673854d51b073a17d08
Saad Sawari <saad1200@gmail.com>
2014-06-15 18:51:15 +0100

    adding karma attempt 1

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var argv = require('yargs').argv;
+var gulp = require('gulp');
+var karma = require('gulp-karma');
+var jshint = require('gulp-jshint');
+var protractor = require('gulp-protractor').protractor;
+
+var testFiles = [
+	  'src/bower_components/angular/angular.js',
+	  'src/bower_components/angular/angular-mocks.js',
+	  'src/lib/**/*.js',
+	  'src/app/app.js',
+	  'src/app/**/*.js',
+	  'spec/**/*.js'
+];
+
+var sourceFiles = ['src/app/**/*.js'];
+
+var acceptanceTestFiles = ['protractor/**/*.js'];
+
+gulp.task('start.test', function() {
+    return gulp.src(testFiles)
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            reporters: ['progress'],
+            autoWatch: true,
+            action: 'start'
+        }));
+});
+
+gulp.task('run.test', function () {
+    return gulp.src(testFiles)
+        .pipe(karma({
+            configFile: 'karma.conf.js',
+            reporters: ['progress'],
+            singleRun: true
+        }));
+});
+
+gulp.task('acceptance', function () {
+var filesPattern = argv.specs ? ('protractor/' + argv.specs)  : 'protractor/**/*.js';
+    return gulp.src(filesPattern)
+        .pipe(protractor({
+            configFile: "protractor.config.js",
+            args: ["--params.reporters.screenshot"]
+        }));
+});
+
+gulp.task('lint', function() {
+    gulp.src(sourceFiles)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch([sourceFiles,testFiles,acceptanceTestFiles], ['lint']);
+});
+
+gulp.task('default', ['lint', 'test', 'watch']);
