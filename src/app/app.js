499d9defc7e350ef282986fee15ae730a5ef9afe
Saad Sawari <saad1200@gmail.com>
2014-06-15 16:36:48 +0100

    project structure

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+//    var common = angular.module('common', []);
+
+    var app = angular.module('app', [
+        // Angular modules 
+        'ngRoute'
+        
+        // Application Modules
+    ]);
+
+})();
