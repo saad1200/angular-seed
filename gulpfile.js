86004c1989ef94fe4869a382f707a48a5866b0d3
Saad Sawari <saad1200@gmail.com>
2014-09-16 00:22:32 +0100

    added gulp task to check server is running

--- gulpfile.js
+++ gulpfile.js
@@ -57,6 +57,36 @@ gulp.task('watch', function () {
   gulp.watch([sourceFiles,testFiles,acceptanceTestFiles], ['lint']);
 });
 
+gulp.task('check.server', function () {
+
+    var http = require('http');
+    var options = {
+      host: 'www.google.com',
+      path: '/index.html'
+    };
+
+    var req = http.get(options, function(res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+      // Buffer the body entirely for processing as a whole.
+      var bodyChunks = [];
+      res.on('data', function(chunk) {
+        // You can process streamed parts here...
+        bodyChunks.push(chunk);
+      }).on('end', function() {
+        var body = Buffer.concat(bodyChunks);
+        console.log('BODY: ' + body);
+        // ...and/or process the entire body here.
+      })
+    });
+
+    req.on('error', function(e) {
+      console.log('ERROR: ' + e.message);
+    });
+    
+});
+
 gulp.task('default', ['lint', 'test', 'watch']);
 
 gulp.on('err', function(e){
