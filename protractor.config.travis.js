e20a2791a649fdff1b4b0403f7cef51a59cc4e32
Saad Sawari <saad1200@gmail.com>
2014-09-16 01:56:28 +0100

    update protractor congig travis

--- protractor.config.travis.js
+++ protractor.config.travis.js
@@ -7,6 +7,18 @@ exports.config = {
     
     specs: ['./userJourneys/*.js'],
     
+    onPrepare: function () {
+			
+        global.wait = function(condition, timeout) {
+            browser.wait(function () { return browser.driver.isElementPresent(condition); }, timeout);
+        };
+        browser.visit = function (url) {
+            browser.manage().window().maximize();
+            return browser.get(url);
+        };
+
+    },
+    
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 30000
