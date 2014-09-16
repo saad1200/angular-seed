fe2908c5c3d95815ec3b1e9c4992544d613d8d4c
Saad Sawari <saad1200@gmail.com>
2014-09-16 03:25:17 +0100

    update deployment script

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,10 +1,6 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
-  #copy data we're interested in to other place
-  cp -R src $HOME/src
-
-
-
+ 
   echo -e "Deployed successfully\n"
 fi
