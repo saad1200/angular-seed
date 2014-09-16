ea9ca9f8244e2d37ed27fdadc34e7e1b06411591
Saad Sawari <saad1200@gmail.com>
2014-09-16 02:14:47 +0100

    added deploy to gh-pages

---
+++ deploy-ghpages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf out || exit 0;
+mkdir out; 
+node build.js
+( cd out
+ git init
+ git config user.name "saad1200
+ git config user.email "saad1200@gmail.com"
+ cp ../CNAME ./CNAME
+ cp ../countryiso.js ./countryiso.js
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
